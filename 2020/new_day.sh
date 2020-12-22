06153b3d43c346320b46db78a978a4cf60ef2146
jobu1233 <maxtrussell@gmail.com>
2020-12-21 19:36:50 -0500

    update new_day script

--- 2020/new_day.sh
+++ 2020/new_day.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-[[ -z "$1" ]] && echo "Usage: new_day.sh <session cookie val>" && exit
+os=$(uname)
+if [ "$os" == "Linux" ]; then
+	cd ~/dev/advent-of-code/2020/
+else
+	cd ~/dev/github/maxtrussell/advent-of-code/2020
+fi
 
-cd ~/dev/github/maxtrussell/advent-of-code/2020
+cookie=''
+if [ -f "cookie.txt" ]; then
+	cookie=$(cat cookie.txt)
+else
+	[[ -z "$1" ]] && echo "Usage: new_day.sh <session cookie val>" && exit
+	cookie=$1
+fi
 
 day=$(date +%d)
 mkdir $day
 url="https://adventofcode.com/2020/day/${day}/input"
-cookies="session=$1"
+cookies="session=$cookie"
 curl -sb $cookies $url > ${day}/input.txt
