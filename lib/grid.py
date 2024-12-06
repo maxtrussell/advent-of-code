43037fd40d0b5687149bb315aa9a0125e580db21
Maxwell Trussell <maxtrussell@gmail.com>
2024-12-06 12:48:03 -0800

    Lib: Add `get()` and setter to Grid

--- lib/grid.py
+++ lib/grid.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -6,39 +9,48 @@ class Point:
     def __hash__(self) -> int:
         return hash((self.x, self.y))
 
-    def __add__(self, other: 'Point') -> 'Point':
+    def __add__(self, other: "Point") -> "Point":
         return Point(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other: 'Point') -> 'Point':
+    def __sub__(self, other: "Point") -> "Point":
         return Point(self.x - other.x, self.y - other.y)
 
-    def __eq__(self, other: 'Point') -> bool:
+    def __eq__(self, other: "Point") -> bool:
         return self.x == other.x and self.y == other.y
 
     def __repr__(self) -> str:
-        return f'Point<{self.x}, {self.y}>'
+        return f"Point<{self.x}, {self.y}>"
+
 
 class Grid:
-    def __init__(self, lines: list[str], diagonal = False):
+    def __init__(self, lines: list[str], diagonal=False):
         self.diagonal = diagonal
         self.grid: list[list[str]] = []
         for line in lines:
             self.grid.append(list(line))
-            
+
     def get_neighbors(self, p: Point) -> list[Point]:
         adjacents = [Point(x, y) for x, y in [(1, 0), (0, 1), (-1, 0), (0, -1)]]
         diagonals = [Point(x, y) for x, y in [(1, 1), (1, -1), (-1, 1), (-1, -1)]]
 
-        neighbors = [p+n for n in adjacents if self.in_bounds(p+n)]
+        neighbors = [p + n for n in adjacents if self.in_bounds(p + n)]
         if self.diagonal:
-            neighbors += [p+n for n in diagonals if self.in_bounds(p+n)]
+            neighbors += [p + n for n in diagonals if self.in_bounds(p + n)]
         return neighbors
 
     def in_bounds(self, p: Point) -> bool:
         return 0 <= p.y < len(self.grid) and 0 <= p.x < len(self.grid[p.y])
 
+    def get(self, p: Point) -> Optional[str]:
+        if self.in_bounds(p):
+            return self[p]
+        return None
+
     def __getitem__(self, p: Point) -> str:
         return self.grid[p.y][p.x]
+
+    def __setitem__(self, p: Point, to: str) -> None:
+        self.grid[p.y][p.x] = to
 
     def __iter__(self):
         for y in range(len(self.grid)):
@@ -46,4 +58,4 @@ class Grid:
                 yield Point(x, y)
 
     def __repr__(self) -> str:
-        return '\n'.join([''.join(row) for row in self.grid])
+        return "\n".join(["".join(row) for row in self.grid])
