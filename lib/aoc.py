aa7e9781a110745ee7bf27e90fd342af468fd645
Maxwell Trussell <maxtrussell@gmail.com>
2025-12-12 09:33:35 -0800

    Add aoc.input_chunks() to read input as sections delimited by empty lines

--- lib/aoc.py
+++ lib/aoc.py
@@ -6,21 +6,32 @@ part = 1
 day = int(os.path.basename(os.path.dirname(sys.argv[0])))
 year = os.path.basename(os.path.dirname(os.path.dirname(sys.argv[0])))
 
+
 @cache
-def input_lines(strip: bool=True) -> list[str]:
+def input_lines(strip: bool = True) -> list[str]:
     global day
-    with open(f'{year}/{day:02}/{sys.argv[1]}', 'r') as f:
+    with open(f"{year}/{day:02}/{sys.argv[1]}", "r") as f:
         if strip:
             return [l.strip() for l in f.readlines()]
         return f.readlines()
 
+
 @cache
 def raw_input() -> str:
     global day
-    with open(f'{day:02}/{sys.argv[1]}', 'r') as f:
+    with open(f"{year}/{day:02}/{sys.argv[1]}", "r") as f:
         return f.read()
+
+
+@cache
+def input_chunks(strip: bool = True) -> list[str]:
+    """Splits input into sections delimited by empty lines."""
+    if strip:
+        return [c.strip() for c in raw_input().split("\n\n")]
+    return raw_input().split("\n\n")
+
 
 def output(ans):
     global part
-    print(f'Part {part}: {ans}')
+    print(f"Part {part}: {ans}")
     part += 1
